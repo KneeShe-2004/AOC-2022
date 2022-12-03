0f8616e6d352f8212e1ae125e06d425d18c7ed51
KneeShe-2004 <77332026+KneeShe-2004@users.noreply.github.com>
2022-12-03 00:52:16 +0000

    Day 2 complete!
    Much simpler and more concise versions of similar code also exist, this just seemed like the fastest way.

---
+++ Day2.py
@@ -0,0 +1,47 @@
+line = ""
+blank = []
+score = 0
+
+
+f = open('Day2-Input.txt', 'r')
+lines = f.readlines()
+numberoflines = len(lines)
+
+f.close()
+
+f = open('Day2-Input.txt', 'r')
+
+for x in range(numberoflines+1):
+    line = f.readline()
+
+    blank = line.split(" ")
+    
+    if blank[0] == 'A' and blank[1] == 'X\n':
+        score = score + 4
+
+    if blank[0] == 'A' and blank[1] == 'Y\n':
+        score = score + 8
+
+    elif blank[0] == 'A' and blank[1] == 'Z\n':
+        score = score + 3
+
+    elif blank[0] == 'B' and blank[1] == 'X\n':
+        score = score + 1
+    
+    elif blank[0] == 'B' and blank[1] == 'Y\n':
+        score = score + 5
+    
+    elif blank[0] == 'B' and blank[1] == 'Z\n':
+        score = score + 9
+    
+    elif blank[0] == 'C' and blank[1] == 'X\n':
+        score = score + 7
+    
+    elif blank[0] == 'C' and blank[1] == 'Y\n':
+        score = score + 2
+    
+    elif blank[0] == 'C' and blank[1] == 'Z\n':
+        score = score + 6
+f.close()
+
+print(score)
